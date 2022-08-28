6fb3cb2a6f2a5141ca45011b789a7dec2cbd21b7
Guru Prasad <guruprasad@Gurus-MacBook-Air.local>
2022-08-28 11:17:44 +0530

    final call

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -12,8 +12,8 @@ module.exports =  (req, res, next)=>
     try {
         const token = req.headers.authorization.split(" ")[1];
         console.log(token);
-        // const verify = jwt.verify(token, 'This is the key')
-        next()
+        const verify = jwt.verify(token, 'This is the key')
+        next(); 
 
     }
     catch(error)
